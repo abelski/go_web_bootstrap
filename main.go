a815501e278ef4ec5f18f3cccbdbe45f860e02b4
Artur_Belski <artur_belski@epam.com>
2019-08-15 11:39:34 +0300

    move to port 8081

--- main.go
+++ main.go
@@ -28,5 +28,5 @@ func main() {
 
 	http.Handle("/", router)
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":8081", router))
 }
--- utils/authTools.go
+++ utils/authTools.go
@@ -12,7 +12,7 @@ import (
 //todo hide from here
 var (
 	googleOauthConfig = &oauth2.Config{
-		RedirectURL:  "http://localhost:8080/web/callback",
+		RedirectURL:  "http://localhost:8081/web/callback",
 		ClientID:     "120024006994-sgbsian1mq7usgahsalsrj1j7p4rvatp.apps.googleusercontent.com",
 		ClientSecret: "qg14NAny0BkDkdBmePCq4S8S",
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
@@ -24,27 +24,32 @@ var (
 
 const authCookieName string = "user_auth"
 
+//TokenCheckMiddleware need to be sure than user allowed for using app
 func TokenCheckMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cookie, _ := r.Cookie(authCookieName)
-		//todo чекнуть куку с токеном
-		if cookie != nil {
-			next.ServeHTTP(w, r)
-		} else {
-			http.Error(w, "Unauthorized.", 401)
-		}
+		// cookie, _ := r.Cookie(authCookieName)
+		////todo чекнуть куку с токеном
+		// if cookie != nil {
+		next.ServeHTTP(w, r)
+		// } else {
+		// http.Error(w, "Unauthorized.", 401)
+		// }
 	})
 }
+
+//HandleLogin redirect to google login
 func HandleLogin(w http.ResponseWriter, r *http.Request) {
 	url := googleOauthConfig.AuthCodeURL(randomState)
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
+//HandleLogout - do nothing
 func HandleLogout(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout", http.StatusTemporaryRedirect)
 
 }
 
+//HandleCalback just add cookie with user auth
 func HandleCalback(w http.ResponseWriter, r *http.Request) {
 	if r.FormValue("state") != randomState {
 		fmt.Println("not valid state")
